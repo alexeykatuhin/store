63ae63458988c381714452f4690302da461dd087
purder <alexeykatuhin@gmail.com>
2017-10-27 13:23:23 +0300

    First Commit

---
+++ WebApplication5/Concrete/EfGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication5.Abstract;
+using WebApplication5.Models;
+
+namespace WebApplication5.Concrete
+{
+	public class EfGameRepository: IItemRepository
+	{
+		GameDataEntities _ctx = new GameDataEntities();
+		public IEnumerable<Item> Items => _ctx.Item;
+	}
+}
---
+++ WebApplication5/Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Abstract;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class ItemController : Controller
+    {
+	    private IItemRepository _repo;
+	    public int pageSize = 4;
+	    public ItemController(IItemRepository repo)
+	    {
+		    _repo = repo;
+	    }
+
+	
+		public ViewResult List(int page = 1)
+		{
+			ItemListViewModel model = new ItemListViewModel()
+			{
+				Items	= _repo.Items
+				.OrderBy(game => game.Id)
+				.Skip((page - 1) * pageSize)
+				.Take(pageSize),
+				PagingInfo = new PagingInfo()
+				{
+					CurrentPage = page,
+					ItemsPerPage = pageSize,
+					TotalItems = _repo.Items.Count()
+				}
+			}; 
+			return View(model);
+		}
+	}
+}
