80aed196c6fd9bbbf1b61e19ed3f5d82674a63b8
Алексей Катухин <avkatukhin@mts.ru>
2021-12-17 14:47:10 +0300

    rusmem com 2

--- WebApplication5/App_Start/BundleConfig.cs
+++ WebApplication5/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace WebApplication5
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include
 						"~/Scripts/jquery.validate*"));
 
 			// Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
