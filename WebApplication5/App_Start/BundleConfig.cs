143b908b9771f2de200b15b18542dc975ecd70a0
Алексей Катухин <avkatukhin@mts.ru>
2021-12-17 14:54:37 +0300

    com

--- WebApplication5/App_Start/BundleConfig.cs
+++ WebApplication5/App_Start/BundleConfig.cs
@@ -14,7 +14,7 @@ namespace WebApplication5
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include
 						"~/Scripts/jquery.validate*"));
 
-			// Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
+			// Используйте версию Modernizr для разработчиков,чтобы учиться работать. Когда вы будете готовы перейти к работе,
 			// используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
